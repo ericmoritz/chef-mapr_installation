6945dc76253055bf72ec3ede0382f0706be75248
Dann S Washko <dwashko@gmti.gannett.com>
2015-06-30 16:06:14 -0400

    GDPHAR-60 - fixing changelog, metadata, adding spec testing, fixing berksfile to use gdp supermarket.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,4 +8,61 @@ describe 'mapr_installation::default' do
   before do
     stub_command("cat /etc/rc.local | grep 'never > /sys/kernel/mm/transparent_hugepage/enabled'").and_return(true)  
   end
+
+  it 'includes recipe mapr_installation::default' do
+    expect(chef_run).to include_recipe("mapr_installation::default")
+  end
+
+  it 'includes recipe mapr_installation::install_prereq_packages' do
+    expect(chef_run).to include_recipe("mapr_installation::install_prereq_packages")
+  end
+
+  it 'includes recipe mapr_installation::iptables' do
+    expect(chef_run).to include_recipe("mapr_installation::iptables")
+  end
+
+  it 'includes recipe mapr_installation::clush' do
+    expect(chef_run).to include_recipe("mapr_installation::clush")
+  end
+
+  it 'includes recipe mapr_installation::validate_host' do
+    expect(chef_run).to include_recipe("mapr_installation::validate_host")
+  end
+
+  it 'includes recipe mapr_installation::mapr_base' do
+    expect(chef_run).to include_recipe("mapr_installation::mapr_base")
+  end
+
+  it 'includes recipe mapr_installation::mapr_nodemanager' do
+    expect(chef_run).to include_recipe("mapr_installation::mapr_nodemanager")
+  end
+
+  it 'includes recipe mapr_installation::mapr_setenv' do
+    expect(chef_run).to include_recipe("mapr_installation::mapr_setenv")
+  end
+
+  it 'includes recipe mapr_installation::mapr_configure' do
+    expect(chef_run).to include_recipe("mapr_installation::mapr_configure")
+  end
+
+  it 'installs package mapr-fileserver' do
+    expect(chef_run).to install_package('mapr-fileserver')
+  end
+
+  it 'installs package mapr-nfs' do
+    expect(chef_run).to install_package('mapr-nfs')
+  end
+
+  it 'installs package bash' do
+    expect(chef_run).to install_package('bash')
+  end
+
+  it 'installs package rpcbind' do
+    expect(chef_run).to install_package('rpcbind')
+  end
+
+  it 'installs package dmidecode' do
+    expect(chef_run).to install_package('dmidecode')
+  end
+
 end

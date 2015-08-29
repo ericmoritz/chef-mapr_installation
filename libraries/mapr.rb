334b861a41c5000c8244d04944b0fa80f5d4d093
Eric Moritz <ericmoritz@gmail.com>
2015-08-29 00:43:22 -0400

    DL-137 removed the redundante mapr_ prefix

--- libraries/mapr.rb
+++ libraries/mapr.rb
@@ -4,11 +4,11 @@ module Mapr
   def self.role?(node, role_name)
     # Returns True if this node should have that role
     fqdn = node['hostalias']
-    role_fqdns = mapr_role_fqdns(node, role_name)
+    role_fqdns = role_fqdns(node, role_name)
     role_fqdns.include?(fqdn)
   end
 
-  def self.mapr_role_fqdns(node, role_name)
+  def self.role_fqdns(node, role_name)
     # extract the role's data if it exists
     role_item = _role_item(node, role_name)
     if role_item.is_a?(Array)

c336e25a652be9d6e33237f47ccf18b8ffde0e71
Eric Moritz <ericmoritz@gmail.com>
2015-10-07 16:28:31 -0400

    DL-186 whoops... forgot to add these file to the previous commit

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'emoritz@gannett.com'
 license          'All rights reserved'
 description      'Installs/Configures mapr_installation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.6'
+version          '0.3.7'
 
 depends 'selinux'
 depends 'ntp'

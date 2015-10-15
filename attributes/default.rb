a4e5700ed439cd9a7c65c89d498552bac171bc39
Rajaram, Hari <hrajaram@gannett.com>
2015-10-14 22:54:45 -0400

    DL-304: fixing space issue,make test works fine in local box but unfortunately in jenkins it failed. Let us c what happens now.

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,6 +27,6 @@ default['mapr']['versions'] = {
   'pig' => '0.14.201509021826-1'
 }
 
-default['mapr']['config']['yarn-site.xml'] = { 'yarn.nodemanager.resource.cpu-vcores'  => '8', 'yarn.nodemanager.resource.memory-mb' => '25000',
+default['mapr']['config']['yarn-site.xml'] = { 'yarn.nodemanager.resource.cpu-vcores' => '8', 'yarn.nodemanager.resource.memory-mb' => '25000',
                                                'yarn.log-aggregation-enable' => 'true', 'yarn.scheduler.maximum-allocation-mb' => '22000'
 }

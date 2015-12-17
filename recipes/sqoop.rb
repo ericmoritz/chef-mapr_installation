950a7841188eb7363d6df0331f30bc66d59a9869
Eric Moritz <ericmoritz@gmail.com>
2015-12-17 10:55:26 -0500

    DL-445 options files generation
    Added code to generate sqoop option files

--- recipes/sqoop.rb
+++ recipes/sqoop.rb
@@ -7,4 +7,18 @@ if role?('sqoop')
   package 'mapr-sqoop' do
     version node['mapr']['versions']['sqoop']
   end
+
+  if node['mapr']['jobs']
+    node['mapr']['jobs'].each do |job_name, job_data|
+      job_data['entities'].each do |entity_name, entity_data|
+        options_content = entity_data['sqoop_options'].join("\n") + "\n"
+        file "/opt/mapr/jobs/#{job_name}/conf/sqoop_#{entity_name}.options" do
+          content options_content
+          owner 'mapr'
+          group 'mapr'
+          mode '664'
+        end
+      end
+    end
+  end
 end
--- spec/sqoop_spec.rb
+++ spec/sqoop_spec.rb
@@ -5,8 +5,22 @@ require 'fauxhai'
 
 describe 'mapr_installation::sqoop' do
   let(:chef_run) {
-    ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').
-      converge(described_recipe)
+    ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5'
+                            ) do |node|
+      node.set['mapr']['jobs']['userStore'] = {
+        'entities' => {
+          'userStore_L1' => {
+            'sqoop_databag' => 'userStore',
+            'sqoop_options' => [
+                'import',
+                '',
+                '--connect',
+                'jdbc:sqlserver://example.com;databaseName=foo'
+              ]
+          }
+        }
+      }
+    end.converge(described_recipe)
   }
   
   it 'should install the sqoop package' do
@@ -17,4 +31,19 @@ describe 'mapr_installation::sqoop' do
                         )
   end
 
+  it 'should create an option file' do
+    expect(chef_run).to create_file(
+                          '/opt/mapr/jobs/userStore/conf/sqoop_userStore_L1.options'
+                        )
+                         .with(
+                           content: %{import
+
+--connect
+jdbc:sqlserver://example.com;databaseName=foo
+},
+                           owner: 'mapr',
+                           group: 'mapr',
+                           mode: '664'
+                         )
+  end
 end
